736abc81c1cb4b731991f1b532639734fa404c17
kojiyamauchi <all.the.small.things@me.com>
2018-10-07 17:11:47 +0900

    update webpack.gulp.babel.js

--- webpack/webpack.gulp.babel.js
+++ webpack/webpack.gulp.babel.js
@@ -36,6 +36,19 @@ module.exports = {
         loader: 'eslint-loader'
       },
       // ES Lint End.
+      // TS Lint.
+      // Compare Editor Settings.
+      {
+        enforce: 'pre',
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: 'tslint-loader',
+        options: {
+          typeCheck: true,
+          fix: true
+        }
+      },
+      // TS Lint End.
       // If Using Riot.js
       {
         test: /\.tag$/,
