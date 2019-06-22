87cb25d235b818d837c3cb125d116e1330e8c085
kojiyamauchi <all.the.small.things@me.com>
2019-06-22 12:02:43 +0900

    update syntax to ver.4 on gulpfile.babel.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,254 +1,205 @@
 'use strict'
-import gulp from 'gulp' // import gulp.
-// Utility Package.
-import gutil from 'gulp-util' // gulp-util plugin.
-import changed from 'gulp-changed' // only change file watch plugin.
-import plumber from 'gulp-plumber' // if error task. don't stop watch plugin.
-import rename from 'gulp-rename' // File Rename PlugIn.
-import del from 'del' // File Delete PlugIn.
+
+// Import Gulp API.
+import { src, dest, lastRun, parallel, watch } from 'gulp'
+
+/*
+ Plugin Modules.
+*/
+// Utilities.
+import utility from 'gulp-util'
+import plumber from 'gulp-plumber'
+import rename from 'gulp-rename'
+import del from 'del'
 // For Webpack.
-import webpack from 'webpack' // webpack.
-import webpackStream from 'webpack-stream' // Using webpack for gulp Plugin.
-import webpackGulp from './webpack/webpack.gulp.babel' // webpack base config file.
+import webpack from 'webpack'
+import webpackStream from 'webpack-stream'
+import webpackGulp from './webpack/webpack.gulp.babel'
 // For JS.
-import jsmin from 'gulp-uglify' // JS File Compression.
-import ejs from 'gulp-ejs' // gulp ejs.
+import jsmin from 'gulp-uglify'
+import ejs from 'gulp-ejs'
 // For Sass & CSS.
-import sass from 'gulp-sass' // sass file compile.
-import sassGlob from 'gulp-sass-glob' // sass glob.
-import postCSS from 'gulp-postcss' // postCSS.
-import autoprefixer from 'autoprefixer' // add vendor prefix in CSS automatically.
-import flexbug from 'postcss-flexbugs-fixes' // for flexbox bug.
-import cssComb from 'gulp-csscomb' // code formatting for CSS.
-import cssmin from 'gulp-cssmin' // CSS File Compression.
-import sourcemaps from 'gulp-sourcemaps' // write sourcemaps.
+import sass from 'gulp-sass'
+import sassGlob from 'gulp-sass-glob'
+import postCSS from 'gulp-postcss'
+import autoprefixer from 'autoprefixer'
+import fixFlexBugs from 'postcss-flexbugs-fixes'
+import csscomb from 'gulp-csscomb'
+import cssmin from 'gulp-cssmin'
 // For HTML.
-import prettify from 'gulp-prettify' // code formatting for HTML.
+import prettify from 'gulp-prettify'
 // For Images.
-import imageMin from 'gulp-imagemin' // images compression.
-import pngImageMin from 'imagemin-pngquant' // png images compression.
-import svgMin from 'gulp-svgmin' // svg compression.
-// For FTP.
-import ftp from 'vinyl-ftp' // ftp plugin.
-import sftp from 'gulp-sftp' // sftp plugin.
-// For BrowserSync.
-import browserSync from 'browser-sync' // browserSync.
-// Setting.
-const autoprefixerSet = [
-  // setting of autoprefixer.
-  'last 2 version',
-  'ie >= 10',
-  'iOS >= 8',
-  'Android >= 4.4'
-]
-const postCSSPlugIn = [autoprefixer({
-  browsers: autoprefixerSet
-}), flexbug] // PostCSS plugin.
-const addIMGDir = 'addImages/*' // added image fold,
-const distIMGDir = 'images/*' // compression image fold,
-const upLoadFile = [
-  // upload file.
+import imgMin from 'gulp-imagemin'
+import pngMin from 'imagemin-pngquant'
+import svgMin from 'gulp-svgmin'
+// For Env.
+import browserSync from 'browser-sync'
+import ftp from 'vinyl-ftp'
+import sftp from 'gulp-sftp'
+
+// Settings.
+const autoprefixerBrowserList = [ 'last 2 version', 'ie >= 10', 'iOS >= 8', ]
+const postCSSPlugIn = [autoprefixer({ browsers: autoprefixerBrowserList }), fixFlexBugs]
+const inImages = 'addImages/*'
+const outImages = 'images/'
+const buildFiles = [
   '**/*.html',
-  '!node_modules/**/*.html',
-  'css/app.min.css',
-  'js/core.min.js',
-  'images/*'
+  'css/**.min.css',
+  'js/**.min.js',
+  'images/**/*',
+  '!node_modules/**/*.html'
 ]
 
-// webpack.
-gulp.task('webpack', () => {
+// Compile JS, Using webpack.
+export const onWebpack = () => {
   return webpackStream(webpackGulp, webpack)
-    .on('error', function () {
-      this.emit('end')
-      console.log('error')
-    })
-    .pipe(gulp.dest('js/'))
-})
-
-// JS File Compression.
-gulp.task('jsmin', () => {
-  return gulp
-    .src('js/*.js', '!/js/*.min.js')
-    .pipe(
-      jsmin({
-        output: {
-          comments: /^!/
-        }
-      })
-    )
-    .pipe(
-      rename({
-        suffix: '.min'
-      })
-    )
-    .pipe(gulp.dest('js/'))
-})
-
-// sass compile & use PostCSS plugIn.
-gulp.task('sass', () => {
-  return gulp
-    .src('sass/**/*.scss')
-    .pipe(
-      plumber({
-        errorHander: error => {
-          console.log(error.message)
-        }
-      })
-    )
-    .pipe(sourcemaps.init())
-    .pipe(sassGlob())
-    .pipe(
-      sass({
-        outputStyle: 'expanded'
-      })
-    )
-    .pipe(postCSS(postCSSPlugIn))
-    .pipe(sourcemaps.write('../maps'))
-    .pipe(cssComb())
-    .pipe(gulp.dest('css/'))
-})
-
-// CSS File Compression.
-gulp.task('cssmin', () => {
-  return gulp
-    .src('css/app.css')
-    .pipe(cssmin())
-    .pipe(
-      rename({
-        suffix: '.min'
-      })
-    )
-    .pipe(gulp.dest('css/'))
-})
-
-// EJS Compile & Code Formatting for HTML.
-gulp.task('ejs', (done) => {
-  return gulp
-    .src(['ejs/**/*.ejs', '!ejs/**/_*.ejs'])
-    .pipe(ejs({}, {}, { ext: '.html' }))
-    .pipe(gulp.dest('.'))
-    .on('end', () => {
-      gulp.src('**/*.html', '!node_modules/**/*.html')
-      .pipe(
-        prettify({
-          indent_size: 2,
-          indent_char: ' ',
-          end_with_newline: false,
-          preserve_newlines: false,
-          unformatted: ['span', 'a', 'img']
-        })
-      )
-      .pipe(gulp.dest('.'))
-      .on('end', done)
-    })
-})
-
-// compression images.
-gulp.task('imgMin', () => {
-  return gulp
-    .src(`${addIMGDir}(.jpg|.jpeg|.png|.gif)`)
-    .pipe(plumber())
-    .pipe(changed(addIMGDir))
-    .pipe(
-      imageMin({
-        use: [
-          pngImageMin({
-            quality: '60-80',
-            speed: 4
-          })
-        ]
-      })
-    )
-    .pipe(gulp.dest(distIMGDir))
-})
-
-// svg file compression.
-gulp.task('svgMin', () => {
-  return gulp
-    .src(`${addIMGDir}.svg`)
-    .pipe(plumber())
-    .pipe(changed(addIMGDir))
-    .pipe(svgMin())
-    .pipe(gulp.dest(distIMGDir))
-})
-
-// File Rename Task.
-gulp.task('rename', () => {
-  return gulp
-    .src('index.html')
-    .pipe(
-      rename({
-        extname: '.php'
-      })
-    )
-    .pipe(gulp.dest('.'))
-})
-
-// File Delete Task.
-gulp.task('delete', cb => {
-  return del(['**/.DS_Store', 'js/*.js', '!/js/*.min.js'], cb)
-})
-
-// local browser connect & sync.
-gulp.task('browserSync', () => {
-  return browserSync({
-    browser: 'google chrome',
-    open: 'external',
-    notify: false,
-    /* if setting proxy.
-    proxy: 'test.dev or localhost:8080'
-    */
-    // setting root.
-    server: {
-      baseDir: '.',
-      index: 'index.html'
-    }
+  .on('error', function () {
+    this.emit('end')
+    console.log('error')
   })
-})
+  .pipe(dest('js/'))
+}
 
-// file save's local browser reload.
-gulp.task('localBrowserReload', () => {
-  return browserSync.reload()
-})
+// Compression JS.
+export const onJsmin = () => {
+  return src('js/*.js', '!/js/*.min.js')
+  .pipe(jsmin())
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(dest('js/'))
+}
 
-// ftp upload.
-gulp.task('ftpUpLoad', () => {
+// Compile sass.
+export const onSass = () => {
+  return src('sass/**/*.scss', { sourcemaps: true })
+  .pipe(plumber({ errorHander: error => console.log(error.message) }))
+  .pipe(sassGlob())
+  .pipe(sass({ outputStyle: 'expanded' }))
+  .pipe(postCSS(postCSSPlugIn))
+  .pipe(csscomb())
+  .pipe(dest('css/', { sourcemaps: '../maps' }))
+}
+
+// Compression CSS.
+export const onCssmin = () => {
+  return src('css/*.css', '!css/*.min.css')
+  .pipe(cssmin())
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(dest('css/'))
+}
+
+// Compile EJS -> Code Formatting for HTML.
+export const onEjs = done => {
+  return src(['ejs/**/*.ejs', '!ejs/**/_*.ejs'])
+  .pipe(ejs({}, {}, { ext: '.html' }))
+  .pipe(dest('.'))
+  .on('end', () => {
+    src('**/*.html', '!node_modules/**/*.html')
+    .pipe(
+      prettify({
+        indent_size: 2,
+        indent_char: ' ',
+        end_with_newline: false,
+        preserve_newlines: false,
+        unformatted: ['span', 'a', 'img']
+      })
+    )
+    .pipe(dest('.'))
+    .on('end', done)
+  })
+}
+
+// Delete Unnecessary Files.
+export const onDelete = cb => {
+  return del(
+    [
+      '**/.DS_Store',
+      'js/*.js',
+      'css/*.css',
+      '**/*.ejs',
+      '!/js/*.min.js',
+      '!css/*.min.css',
+      '!ejs/**/*',
+    ], cb)
+}
+
+// Compression Image Files.
+export const onImgmin = () => {
+  return src(`${inImages}(.jpg|.jpeg|.png|.gif)`, { since: lastRun(onImgmin) })
+  .pipe(plumber())
+  .pipe(imgMin({ use: [ pngMin({ quality: '60-80', speed: 4 }) ]}))
+  .pipe(dest(outImages))
+}
+
+// Compression SVG Files.
+export const onSvgmin = () => {
+  return src(`${inImages}.svg`, { since: lastRun(onSvgmin) })
+  .pipe(plumber())
+  .pipe(svgMin())
+  .pipe(dest(outImages))
+}
+
+// When Renaming Files.
+export const onRename = () => {
+  return src('addFile.name')
+  .pipe(rename({ extname: '.extension' }))
+  .pipe(dest('.'))
+}
+
+// When File Uploading on Server by FTP.
+export const onFtpUpLoad = () => {
   const ftpConnect = ftp.create({
     host: '***',
     user: '***',
     password: '***',
     parallel: 7,
-    log: gutil.log
+    log: utility.log
   })
-  return gulp
-    .src(upLoadFile, {
-      base: '.',
-      buffer: false
-    })
-    .pipe(ftpConnect.newer('/'))
-    .pipe(ftpConnect.dest('/'))
+  return src(buildFiles, { base: '.', buffer: false })
+  .pipe(ftpConnect.newer('/'))
+  .pipe(ftpConnect.dest('/'))
+}
+
+// Start Up Local Browser.
+export const onBrowserSync = () => {
+  return browserSync({
+    browser: 'google chrome',
+    open: 'external',
+    notify: false,
+    /* if Setting Proxy.
+    proxy: 'test.dev or localhost:8080'
+    */
+    // Setting Root.
+    server: { baseDir: '.', index: 'index.html' }
+  })
+}
+
+// Default Tasks.
+exports.default = parallel( onBrowserSync, () => {
+  if(switches.webpack) watch('base/**/*', onWebpack)
+  if(switches.jsmin) watch(['js/*.js', '!/js/*.min.js'], onJsmin)
+  if(switches.sass) watch('sass/**/*.scss', onSass)
+  if(switches.cssmin) watch(['css/*.css', '!css/*.min.css'], onCssmin)
+  if(switches.ejs) watch('ejs/**/*', onEjs)
+  if(switches.delete) watch(buildFiles, onDelete)
+  if(switches.imgmin) watch(inImages, parallel(onImgmin, onSvgmin))
+  if(switches.rename) watch('**/*', onRename)
+  if(switches.ftp) watch(buildFiles, onFtpUpLoad)
+  let timeID
+  watch(buildFiles).on('change',() => {
+    clearTimeout(timeID)
+    timeID = setTimeout(() => browserSync.reload(), 1000)
+  })
 })
 
-// gulp default task, terminal command 'gulp'.
-gulp.task('default', gulp.parallel( 'browserSync', () => {
-  // first task, local server connect & local browser sync.
-  // ↓Select a task according to the project. プロジェクトで使用するタスクを選択しましょう。↓
-  // gulp.watch('base/**/*', gulp.task('webpack')) // JS File webpack.
-  // gulp.watch('js/core.js', gulp.task('jsmin')) // watching change's JS flie, File Compression.
-  // gulp.watch('sass/**/*.scss', gulp.task('sass')) // watching sass file save's auto compile & add vendor prefix automatically.
-  // gulp.watch('css/app.css', gulp.task('cssmin')) // watching change's CSS flie, File Compression.
-  // gulp.watch('ejs/**/*', gulp.task('ejs')) // watch ejs.
-  // gulp.watch(addIMGDir, gulp.parallel('imgMin', 'svgMin')) // watching Img Dir compression.
-  // gulp.watch('**/*', gulp.task('rename')) // watching Rename Task.
-  // gulp.watch('**/*', gulp.task('delete')) // watching Delete Task.
-  // gulp.watch(upLoadFile, gulp.task('ftpUpLoad')) // watching file save's auto ftp upload.
-  /*
-  let timeID // watching file save's local browser reload & adjusted reload start time.
-  gulp.watch(upLoadFile).on('change',() => {
-    clearTimeout(timeID)
-      timeID = setTimeout(() => {
-        browserSync.reload()
-      }, 1000)
-    })
-  */
-}))
+// Switches.
+const switches = {
+  webpack: true,
+  jsmin: true,
+  sass: true,
+  cssmin: true,
+  ejs: false,
+  delete: true,
+  imgmin: false,
+  rename: false,
+  ftp: false
+}
